5c49c6b30f27c37bad1080237d3b18d9b11a6411
souweiba maiga <maigasouweiba932615@gmail.com>
2026-04-24 14:41:27 +0100

    feat: ajout de la fonction racine

--- calculatrice.js
+++ calculatrice.js
@@ -16,6 +16,18 @@ function division (a,b){
      return a/b
 }
 
+function racineCarree(n) {
+if (n < 0) {
+throw new Error('Impossible : racine carrée d un nombre négatif !');
+}
+return Math.sqrt(n);
+}
+console.log('√16 =', racineCarree(16));    
+// Attendu : 4
+console.log('√2 ≈', racineCarree(2));      
+// Attendu : 1.4142...
+
+
 console.log('5 + 3 =', additionner(5, 3));
 console.log('10 - 4 =', soustraire(10, 4));
 console.log('6 * 7 =', multiplier(6, 7));
