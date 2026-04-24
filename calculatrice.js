0fc381c89dd6530e576ae1d3b1f04b1bcf06cd19
SALAMOU ABDOU Lourwana <larwanaabdousalam@gmail.com>
2026-04-24 14:33:09 +0100

    initial:ajout de la fonction moduo

--- calculatrice.js
+++ calculatrice.js
@@ -15,6 +15,15 @@ function division (a,b){
     throw new console.error('Division par zero impossible');
      return a/b
 }
+function modulo(a, b) {
+    if (b === 0) {
+        throw new Error('Modulo par zéro impossible !');
+    }
+    return a % b;
+}
+// Tester la fonction
+console.log('10 % 3 =', modulo(10, 3));   // Attendu : 1
+console.log('15 % 4 =', modulo(15, 4));   // Attendu :
 
 console.log('5 + 3 =', additionner(5, 3));
 console.log('10 - 4 =', soustraire(10, 4));
